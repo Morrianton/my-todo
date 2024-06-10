dc4912ea65ad87dffc636d0c04d72feeb73d3a94
Bryson Smith <morrianton@gmail.com>
2024-06-10 11:06:44 -0700

    Added email and password validation to signup.

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import argon2 from "argon2";
+import validator from "validator";
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,18 @@ const userSchema = new Schema({
 
  // static signup method
  userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled.');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error('Email is invalid.');
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough.');
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) throw Error('Email already in use.');
