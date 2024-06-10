a79806e081c830186770fb104cea4adefd7ce4c5
Bryson Smith <morrianton@gmail.com>
2024-06-10 11:47:17 -0700

    Added business logic for getting a user by ID, getting all users, and deleting a user by ID.

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,5 +1,7 @@
-import User from "../models/userModel.mjs";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+import User from "../models/userModel.mjs";
 
 /**
  * Creates a JSON Web Token.
@@ -25,7 +27,7 @@ export const loginUser = async (req, res) => {
  */
 export const signupUser = async (req, res) => {
   const { email, password } = req.body;
-
+  
   try {
     const user = await User.signup(email, password);
     const token = createToken(user._id);
@@ -34,7 +36,68 @@ export const signupUser = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+/**
+ * Gets a single user from the database by its ID. 
+ * @param {Request}  req Request object.
+ * @param {Response} res Response object.
+ */
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'User ID is invalid.' });
+  }
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.'});
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
+/**
+ * Gets all the users from the database.
+ * @param {Request} req Request object.
+ * @param {Response} res Response object.
+ */
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    if (!users) return res.status(404).json({ error: 'No users were found.'});
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+/**
+ * Deletes a single user from the database.
+ * @param {Request}  req Request object.
+ * @param {Response} res Response object.
+ */
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'User ID is invalid.'})
+  }
+
+  try {
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+
+    if (!deleteUser) return res.status(404).json({ error: 'User not found.'});
+
+    res.status(200).json({ deletedUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
