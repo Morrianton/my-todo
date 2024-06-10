f09ceb9f3ddf16f4ee8f97f40bbebb767739986c
Bryson Smith <morrianton@gmail.com>
2024-06-10 11:48:02 -0700

    Added get user ID and delete user by ID routes.

--- server/routes/user.mjs
+++ server/routes/user.mjs
@@ -1,11 +1,20 @@
 import express from 'express';
 
-import { loginUser, signupUser } from '../controllers/userController.mjs';
+import {
+  deleteUser,
+  getUser,
+  loginUser,
+  signupUser
+} from '../controllers/userController.mjs';
 
 const router = express.Router();
+
+router.get('/:id', getUser);
 
 router.post('/login', loginUser);
 
 router.post('/signup', signupUser);
+
+router.delete('/:id', deleteUser);
 
 export default router;
