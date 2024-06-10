1cba51a46e16795ca271d0a6acafed150cab4fea
Bryson Smith <morrianton@gmail.com>
2024-06-10 11:47:33 -0700

    Whitespace changes.

--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -18,17 +18,11 @@ const userSchema = new Schema({
 
  // static signup method
  userSchema.statics.signup = async function (email, password) {
-  if (!email || !password) {
-    throw Error('All fields must be filled.');
-  }
+  if (!email || !password) throw Error('All fields must be filled.');
 
-  if (!validator.isEmail(email)) {
-    throw Error('Email is invalid.');
-  }
+  if (!validator.isEmail(email)) throw Error('Email is invalid.');
 
-  if (!validator.isStrongPassword(password)) {
-    throw Error('Password is not strong enough.');
-  }
+  if (!validator.isStrongPassword(password)) throw Error('Password is not strong enough.');
 
   const exists = await this.findOne({ email });
 
