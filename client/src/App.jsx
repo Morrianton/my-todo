fff157557ed0d09ad923693c29308cd899ad0d57
Bryson Smith <morrianton@gmail.com>
2024-06-14 15:45:04 -0700

    Removed fetch logic for lists.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,37 +10,17 @@ import AuthContext from "./contexts/Auth.context";
 import AuthReducer from "./reducers/Auth.reducer";
 
 function App() {
-  const abortController = new AbortController();
   const [user, dispatchAuth] = useReducer(AuthReducer, null);
-
-  useEffect(() => {
-    axios(`/api/v1/users/${user._id}`, { signal: abortController.signal })
-    .then((response) => {
-      if (response.statusText === 'OK') {
-        dispatchAuth({ type: 'SET_USERS', payload: response.data });
-      }
-    })
-    .catch((error) => {
-      if (error.name === 'AbortError') {
-        console.log('Fetch aborted');
-      } else {
-        console.error(error)
-      }
-    });
-
-    return () => abortController.abort();
-  }, []);
 
   return (
     <AuthContext.Provider value={ user }>
-    <Router>
-      <Routes>
-        <AuthContext.Provider value={ user }>
-          <Route path="/" element={ <TasksPage /> } />
-          <Route path="/login" element={ <LoginPage /> } />
+      <Router>
+        <Routes>
+            <Route path="/" element={ <TasksPage /> } />
+            <Route path="/login" element={ <LoginPage /> } />
             <Route path="/signup" element={ <SignupPage /> } />
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
     </AuthContext.Provider>
   );
 }
