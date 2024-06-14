c2a93dc116ae4c63e6c4756367ed4074720e3772
Bryson Smith <morrianton@gmail.com>
2024-06-14 15:52:31 -0700

    Filled in log in page component with simple templating and logic.

--- client/src/pages/Login.page.jsx
+++ client/src/pages/Login.page.jsx
@@ -1,5 +1,31 @@
-function LoginPage() {
-  return <><p>Welcome to the login page!</p></>;
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Log in</h3>
+      <label>Email</label>
+      <input
+        type="email"
+        onChange={ (event) => setEmail(e.target.value) }
+        value={ email }
+      />
+      <label>Password</label>
+      <input
+        type="email"
+        onChange={ (event) => setPassword(e.target.value) }
+        value={ password }
+      />
+      <button>Log in</button>
+    </form>
+  )
 }
 
 export default LoginPage;
