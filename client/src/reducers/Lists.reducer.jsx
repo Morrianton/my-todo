78ace78cdd7a679ef16f480ff3f1d83a93f35f06
Bryson Smith <morrianton@gmail.com>
2024-06-15 00:06:23 -0700

    Fixed bug where lists set are the array elements instead of the object.

--- client/src/reducers/Lists.reducer.jsx
+++ client/src/reducers/Lists.reducer.jsx
@@ -7,13 +7,12 @@
 export default (state, action) => {
   switch(action.type) {
     case 'SET_LISTS':
-      return [action.payload];
+      return [...action.payload];
     case 'CREATE_LIST':
       return [action.payload, ...state];
     case 'DELETE_LIST':
       return state.filter((list) => list._id !== action.payload._id);
     case 'UPDATE_LIST':
-      console.log(state)
       return [action.payload, ...state.filter((list) => list._id !== action.payload._id)];
     };
     throw new Error(`Unknown action: ${action.type}`);
