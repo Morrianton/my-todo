0f748ba41456fede9c1ff22450d96b8a33503c52
Bryson Smith <morrianton@gmail.com>
2024-06-26 22:20:49 -0700

    Added more logic and filled out template a bit more.

--- client/src/pages/AccountSettings.page.jsx
+++ client/src/pages/AccountSettings.page.jsx
@@ -1,39 +1,62 @@
 import { useState } from "react";
+import axios from "axios";
 
 /**
+ * Account Settings Page component.
  * 
- * @returns {JSX.Element} The account settings page component to render.
+ * @component
+ * @returns {React.JSX.Element} The account settings page component to render.
  */
 const AccountSettingsPage = () => {
-  const [entry, setEntry] = useState('');
   const [error, setError] = useState(null);
+  const [messageSent, setMessageSent] = useState(false);
+  const [pending, setPending] = useState(false);
+  const [recipientEmail, setRecipientEmail] = useState('');
 
   /**
-   * 
+   * Sends an email invitation to sign up to use the app.
+   * @param {Event} event Event object to prevent default form submission behavior.
    */
   const sendInvitation = (event) => {
     event.preventDefault();
-    // send off email
+    setPending(true);
+
+    axios.post(
+      '/api/v1/settings/invite',
+      { email: recipientEmail },
+      { headers: { Authorization: user.token } }
+    )
+    .then((response) => {
+      if (response.statusText === 'OK') {
+        setTimeout(() => {
+          setRecipientEmail('');
+          setMessageSent(false);
+        }, 5000);
+      }
+    })
+    .catch((error) => setError(error))
+    .finally(() => setPending(false));
   };
 
   return (
     <>
       <h1>Account Settings</h1>
       <h2>Social</h2>
+      <p>Invite a friend to use the My To-do app.</p>
       <form action={sendInvitation}>
         <label>
-          recipient's email
+          Email
           <input
-            onChange={(event) => setEntry(event.target.value)}
+            onChange={(event) => setRecipientEmail(event.target.value)}
             placeholder="genie@ofthelamp.net"
             type="email"
-            value={entry}
+            value={recipientEmail}
           />
         </label>
-        <button type="submit">Send</button>
-        { error && <p>{error}</p> }
+        <button disabled={pending} type="submit">Invite</button>
+        { error && <p>{error.message}</p> }
+        { messageSent && <p>Hurray! We've sent an invitation to {recipientEmail}!</p> }
       </form>
-
     </>
   );
 }
